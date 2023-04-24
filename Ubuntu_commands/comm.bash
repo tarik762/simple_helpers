8cc9e7304a361e84707ae83040eef70678093ff4
Taras Melnykov <tarik762@ukr.net>
2023-04-24 09:27:56 +0300

    Last changes

---
+++ Ubuntu_commands/comm.bash
@@ -0,0 +1,9 @@
+#почистити систему від непотрібних версій ядра
+apt remove $(dpkg-query --show 'linux-modules-*' | cut -f1 | grep -v "$(uname -r)") 
+
+#видалити старі версії СНАПА - це скрипт
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
